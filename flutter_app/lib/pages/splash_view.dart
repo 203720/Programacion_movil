3e2e4aaf1b86289cfcb7b55e0988133dc7e91ef9
Azul Briones <203720@ids.upchiapas.edu.mx>
2023-02-13 00:28:46 -0600

    SplashView to login added

--- flutter_app/lib/pages/splash_view.dart
+++ flutter_app/lib/pages/splash_view.dart
@@ -1,10 +1,39 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/login.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(context, MaterialPageRoute(
+      builder: (context) => const Login()
+    ));
+  }
+
+  @override
   Widget build(BuildContext context) {
+    return initWidget(context);
+  }
+
+  Widget initWidget(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
